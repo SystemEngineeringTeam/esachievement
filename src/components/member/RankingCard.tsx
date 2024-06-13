3c0a473947e3ce667c5f9ef776f6ec9727b0813b
kinako1415 <Owata09@outlook.jp>
2024-06-14 02:38:23 +0900

    RankingCardの情報の表示

--- src/components/member/RankingCard.tsx
+++ src/components/member/RankingCard.tsx
@@ -1,13 +1,28 @@
 import { Member } from "@/types/member";
-import { Text } from "@radix-ui/themes";
+import { Avatar, Box, Flex, Text } from "@radix-ui/themes";
 import { type ReactElement } from "react";
 
 export function MemberCard({ member }: { member: Member }): ReactElement {
   return (
     <>
-      <Text>Hello This page is memberCard</Text>
-      <Text>{member.name}</Text>
-      <Text>{member.email}</Text>
+      <Box maxWidth="240px">
+        <Flex gap="3" align="center">
+          <Avatar
+            size="3"
+            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            radius="full"
+            fallback="T"
+          />
+          <Box>
+            <Text as="div" size="2" weight="bold">
+              {member.name}
+            </Text>
+            <Text as="div" size="2" color="gray">
+              Engineering
+            </Text>
+          </Box>
+        </Flex>
+      </Box>
     </>
   );
 }
