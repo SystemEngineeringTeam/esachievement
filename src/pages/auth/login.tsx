b4415a0273578a3af3c252b05a09748c187bd59e
nasubi <r916nis@gmail.com>
2024-06-15 23:06:29 +0900

    .envの用意とButtonを用意

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -6,7 +6,12 @@ export default function Page(): ReactElement {
     <Flex direction="column" gap="2">
       <Text>こちらログイン画面になります</Text>
       <a
-        href={`https://api.esa.io/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_LOCAL_URI}&scope=read+write&response_type=code`}
+        href={`https://api.esa.io/oauth/authorize?client_id=${import.meta.env.VITE_LOCAL_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_LOCAL_REDIRECT_URI}&scope=read+write&response_type=code`}
+      >
+        <Button>Local Login</Button>
+      </a>
+      <a
+        href={`https://api.esa.io/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&redirect_uri=${import.meta.env.VITE_REDIRECT_URI}&scope=read+write&response_type=code`}
       >
         <Button>Login</Button>
       </a>
