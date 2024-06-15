ec91f2f3556363f1d5b09df9d816621e1460428e
1500hayao <169636853+1500hayao@users.noreply.github.com>
2024-06-15 22:06:22 +0900

    hayato.tsxの内容をInfoに移動

--- src/pages/hayato.tsx
+++ src/pages/hayato.tsx
@@ -1,38 +1,8 @@
-import { Flex, Text, Avatar, Box} from "@radix-ui/themes";
 import { type ReactElement } from "react";
+import Info from "@/components/achievements/Info";
 
 export default function Page(): ReactElement {
-  const achievementTest = {
-    id: 1,
-    name: "first blood",
-    description: "Get the first kill in a match.",
-    icon: "https://placehold.jp/150x150.png",
-    tag: ["kill"],
-  };
-
   return (
-
-    <Box maxWidth="8rem" >
-      <Flex direction="column" align="center">
-
-          <Avatar
-            size="7"
-            src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-            radius="full"
-            fallback="T"
-            />
-
-        <Text size="6" weight="bold">
-          恋愛失敗
-        </Text>
-
-      </Flex>
-
-          {achievementTest.tag.map((tag,index) => (
-            <Text key={index} color="gray">タグ {tag}</Text>
-          ))}
-
-    </Box>
-
+   <Info />
   );
 }
