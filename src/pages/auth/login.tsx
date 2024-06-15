133516436298541b4a8554f43b7d2eb38dd9c594
nasubi <r916nis@gmail.com>
2024-06-15 15:47:01 +0900

    linkの修正

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,17 +1,15 @@
 import { Flex, Button, Text } from "@radix-ui/themes";
-import { type ReactElement, useState } from "react";
+import { type ReactElement } from "react";
 
-export default function App(): ReactElement {
-
+export default function Page(): ReactElement {
   return (
     <Flex direction="column" gap="2">
       <Text>こちらログイン画面になります</Text>
-      <a href="https://api.esa.io/">
-      <Button
+      <a
+        href={`https://api.esa.io/oauth/authorize?client_id=${import.meta.env.CLIENT_ID}&redirect_uri=${import.meta.env.REDIRECT_URI}&scope=read+write&response_type=code`}
       >
-        Login
-      </Button>
-        </a>
+        <Button>Login</Button>
+      </a>
     </Flex>
   );
 }
