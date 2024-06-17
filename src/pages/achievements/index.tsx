a4592247b0cf5e4b815923e3766e718bd298f72c
1500hayao <169636853+1500hayao@users.noreply.github.com>
2024-06-17 11:39:56 +0900

    achievementsでfirstpenguinの表示

--- src/pages/achievements/index.tsx
+++ src/pages/achievements/index.tsx
@@ -1,10 +1,12 @@
 import { type ReactElement } from "react";
+import { FirstPenguin } from "@/components/achievements/FirstPenguin";
 import { AchievementCard } from "@/components/achievements/Card";
 import data from "@/assets/achievements.json";
 
 export default function Page(): ReactElement {
   return (
     <>
+      <FirstPenguin />
       {data.achievements.map((achievement) => (
         <AchievementCard
           key={achievement.id}
