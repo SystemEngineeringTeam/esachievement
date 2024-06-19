a56b2a971194f3c3ddd1d7c31ee55124c589857c
wappon28dev <wappon_28_dev@ruru.be>
2024-06-19 14:03:10 +0900

    💚 `/functions` をデプロイするように

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_ESA_APP_CLIENT_ID: string;
-  readonly VITE_ESA_APP_CLIENT_SECRET: string;
-  readonly VITE_ESA_APP_REDIRECT_URI: string;
-}
+import { Env } from "./types/env";
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: Env;
 }
