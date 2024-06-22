2d26af8e1741e4aa3277c64910c2e5acfa063874
nrak126 <daichidaichi126@gmail.com>
2024-06-22 23:17:53 +0900

    logRecentUnrockedからachievements/[id]に飛ぶ

--- src/components/ranking/LogRecentUnlocked.tsx
+++ src/components/ranking/LogRecentUnlocked.tsx
@@ -3,6 +3,7 @@ import { type ReactElement } from "react";
 import styled from "styled-components";
 import SampleAchievements from "@/assets/achievements.json";
 import SampleMembers from "@/assets/members.json";
+import { Link } from "@/router";
 import { type Achievement } from "@/types/achievement";
 import { type Member } from "@/types/member";
 
@@ -30,23 +31,20 @@ export function LogRecentUnlocked({
     (a) => a.id === achievementID,
   );
   return (
-    <BoxStyle>
-      <Flex align="center" gap="3" justify="center">
-        <Avatar
-          fallback="T"
-          radius="full"
-          size="4"
-          src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
-        />
-        <Box>
-          <Text as="div" size="3" weight="bold">
-            {member?.name}
-          </Text>
-          <Text as="div" color="gray" size="3">
-            {achievement.name}
-          </Text>
-        </Box>
-      </Flex>
-    </BoxStyle>
+    <Link params={{ id: achievement.id.toString() }} to="/achievements/:id">
+      <BoxStyle>
+        <Flex align="center" gap="3" justify="center">
+          <Avatar fallback="T" radius="full" size="4" src={achievement.icon} />
+          <Box>
+            <Text as="div" size="3" weight="bold">
+              {member.name}
+            </Text>
+            <Text as="div" color="gray" size="3">
+              {achievement.name}
+            </Text>
+          </Box>
+        </Flex>
+      </BoxStyle>
+    </Link>
   );
 }
