307d4043a19ebfe6e43d4e7c1b5328a045ff25f8
nasubi <r916nis@gmail.com>
2024-06-23 00:23:10 +0900

    nullable → optional

--- src/types/post-data/_struct.ts
+++ src/types/post-data/_struct.ts
@@ -4,7 +4,7 @@ import { type Override, type Nullable } from "@/types/utils";
 export const yPostData = object().shape({
   _name: string().oneOf(["esachievement"]).required(),
   _version: mixed().oneOf(["1"]).required(),
-  data: mixed().nullable(),
+  data: mixed().optional(),
 });
 
 export type PostData<T> = Override<
