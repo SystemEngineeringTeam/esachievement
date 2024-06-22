b144fb606a6966e8a54790e0303f860ace96054f
nrak126 <daichidaichi126@gmail.com>
2024-06-22 18:48:44 +0900

    RankingCardのアイコンにmatchMemberのものを表示

--- src/components/member/RankingCard.tsx
+++ src/components/member/RankingCard.tsx
@@ -34,7 +34,7 @@ export function RankingCard({
           fallback="T"
           radius="full"
           size="8"
-          src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+          src={matchMember.icon}
         />
         <Flex align="center" direction="column">
           <Text as="div" size="7" weight="bold">
