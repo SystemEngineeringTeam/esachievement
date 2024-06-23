a1a31b32b920c13e31e8547abea6cf20d9a9983d
nrak126 <daichidaichi126@gmail.com>
2024-06-23 10:25:22 +0900

    recentUnlockedCardの表示

--- src/components/achievements/RecentUnlockedCard.tsx
+++ src/components/achievements/RecentUnlockedCard.tsx
@@ -1,40 +1,80 @@
 import { Avatar, Flex, Table, Text } from "@radix-ui/themes";
-import { type ReactElement } from "react";
+import { useEffect, useState, type ReactElement } from "react";
 import Achievements from "@/assets/achievements.json";
 import { type UnlockedAchievement } from "@/types/post-data/unlocked-achievements";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export function RecentUnlockedCard({
-  achievement,
+  unlockedAchievement,
 }: {
-  achievement: UnlockedAchievement;
+  unlockedAchievement: UnlockedAchievement;
 }): ReactElement {
-  const matchedUnlockedAchievement = Achievements.achievements.find(
-    (a) => a.id.toString() === achievement.memberEmail,
+
+	const navigate = useNavigate();
+  const TableRow = styled(Table.Row)`
+    transition: background-color 100ms;
+    cursor: pointer;
+    &:hover {
+      background-color: #e2e8f0;
+    }
+  `;
+
+  const [dayDiff, setDayDiff] = useState(0);
+  const rtf1 = new Intl.RelativeTimeFormat("jp-lg", { style: "short" });
+
+  const matchedAchievement = Achievements.achievements.find(
+    (a) => a.id === unlockedAchievement.achievementID,
   );
+  if (matchedAchievement == null) {
+    throw new Error("matchedAchievement not found!");
+  }
+
+  useEffect(() => {
+    const now = new Date();
+    const formatedCreatedAt = new Date(matchedAchievement.createdAt);
+    setDayDiff(
+      Math.round(
+        (formatedCreatedAt.getTime() - now.getTime()) / (24 * 60 * 60 * 1000),
+      ),
+    );
+  });
 
   return (
-    <Table.Row>
+    <TableRow
+      onClick={() => {
+        navigate("/achievements/:id", {
+          params: {
+            id: matchedAchievement.id.toString(),
+          },
+        });
+      }}
+    >
       <Table.RowHeaderCell>
+        <Text>{rtf1.format(dayDiff, "day")}</Text>
+        {/* <Text>{passedDate}</Text> */}
+      </Table.RowHeaderCell>
+      <Table.Cell>
         <Flex gap="2">
           <Avatar
             fallback="A"
             radius="full"
             size="6"
-            src={matchedUnlockedAchievement?.icon}
+            src={matchedAchievement?.icon}
           />
         </Flex>
-      </Table.RowHeaderCell>
+      </Table.Cell>
       <Table.Cell>
         <Text as="div" size="8" weight="bold">
-          {matchedUnlockedAchievement?.name}
+          {matchedAchievement?.name}
         </Text>
       </Table.Cell>
-      <Table.Cell>{matchedUnlockedAchievement?.description}</Table.Cell>
+      <Table.Cell>{matchedAchievement?.description}</Table.Cell>
       <Table.Cell>
         <Text as="div" size="6">
-          #{matchedUnlockedAchievement?.tags[0].name}
+          #{matchedAchievement?.tags[0].name}
         </Text>
       </Table.Cell>
-    </Table.Row>
+    </TableRow>
   );
 }
--- src/pages/members/[id]/index.tsx
+++ src/pages/members/[id]/index.tsx
@@ -2,8 +2,10 @@ import { Box, Table } from "@radix-ui/themes";
 import { type ReactElement } from "react";
 import styled from "styled-components";
 import UnlockedAchievements from "@/assets/unlockedAchievements.json";
+import { FirstPenguin } from "@/components/achievements/FirstPenguin";
 import { RecentUnlockedCard } from "@/components/achievements/RecentUnlockedCard";
 import { useParams } from "@/router";
+import { type UnlockedAchievement } from "@/types/post-data/unlocked-achievements";
 
 const BoxStyle = styled(Box)`
   margin: 0 auto;
@@ -13,33 +15,37 @@ export default function Page(): ReactElement {
   // const { params } = useMatch('/posts/$id')
   const { id } = useParams("/achievements/:id");
 
-  const memberRecentUnlocked =
-    UnlockedAchievements.unlockedAchievements.filter(
-      (a) => a.memberEmail === id,
-    );
+  const memberRecentUnlocked = UnlockedAchievements.unlockedAchievements.filter(
+    (a) => a.memberEmail === id,
+  );
   // memberRecentUnlocked.sort((a, b) => b.createdAt - a.createdAt)
 
   return (
-    <BoxStyle width="70%">
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell> </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>名前</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>説明</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>タグ</Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
+    <>
+      <FirstPenguin />
+      <BoxStyle width="70%">
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell> </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>名前</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>説明</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>タグ</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
 
-        <Table.Body>
-          {memberRecentUnlocked.map((achievement) => (
-            <RecentUnlockedCard
-              key={achievement.memberEmail}
-              achievement={achievement}
-            />
-          ))}
-        </Table.Body>
-      </Table.Root>
-    </BoxStyle>
+          <Table.Body>
+            {memberRecentUnlocked.map((achievement) => (
+              <RecentUnlockedCard
+                key={achievement.memberEmail}
+                unlockedAchievement={
+                  achievement as unknown as UnlockedAchievement
+                }
+              />
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </BoxStyle>
+    </>
   );
 }
