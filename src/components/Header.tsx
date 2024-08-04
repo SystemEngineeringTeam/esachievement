f7c0aae4bf97de46a32eda848f54e6b14689bb5d
kinako1415 <Owata09@outlook.jp>
2024-08-04 13:29:07 +0900

    ヘッダーのスタイルを調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,102 +7,109 @@ import { $hasAuthenticated } from "@/lib/stores/auth";
 import { Link } from "@/router.ts";
 
 const HeaderStyle = styled(Flex)`
-  background-color: #f1f5f9;
-  height: 4.8rem;
+  margin: 0 auto;
+  height: 9%;
+  width: 94%;
+`;
 
-  .esaAchievementsStyle {
-    margin-right: auto;
-  }
-  .createStyle {
-    color: #f8fafc;
-    background-color: #0f172a;
-    align-items: center;
-    justify-content: center;
-    width: 13rem;
-    display: flex;
-  }
-  :hover.createHoverAnime {
+const RightContents = styled(Flex)`
+  margin-left: auto;
+  border-radius: 80px;
+`;
+
+const CreateStyle = styled(Link)`
+  color: #f8fafc;
+  background-color: #0f172a;
+  align-items: center;
+  justify-content: center;
+  width: 13rem;
+  border-radius: 80px;
+  display: flex;
+  &:hover {
     background: #1e293b;
-  }
-  .loginStyle {
-    width: 15rem;
-    color: #f8fafc;
-    align-items: center;
-    display: flex;
-    justify-content: center;
-    width: 13rem;
-    background-color: #0f172a;
   }
 `;
 
 const LinkContents = styled(Link)`
+  background: #dadfe2;
   height: 100%;
-  display: flex;
-  padding: 0 1rem;
+  padding: 0 5rem 0 2rem;
   align-items: center;
+  justify-items: center;
+  display: flex;
+  margin-right: -4rem;
+  border-radius: 80px;
   &:hover {
-    background: #e2e8f0;
+    background: #dadfe2;
   }
 `;
+
+const EsaAchievementsStyle = styled(Link)``;
 
 export function Header(): ReactElement {
   return (
     <HeaderStyle>
-      <LinkContents className="esaAchievementsStyle" to="/">
+      <EsaAchievementsStyle to="/">
         <Text size="7" weight="bold">
-          えさちぶめんと
+          esachievement
         </Text>
-      </LinkContents>
-      <LinkContents to="/ranking">
-        <Text as="div" size="5" weight="bold">
-          ranking
-        </Text>
-      </LinkContents>
-      <LinkContents to="/members">
-        <Text size="5" weight="bold">
-          members
-        </Text>
-      </LinkContents>
-      <LinkContents to="/achievements">
-        <Text size="5" weight="bold">
-          achievements
-        </Text>
-      </LinkContents>
-      {useStore($hasAuthenticated) ? (
-        <Link
-          className="createStyle createHoverAnime headerDefaultStyle"
-          to="/unlocked"
-        >
-          <Flex align="center" gap="4">
-            <Text size="5" weight="bold">
-              実績管理
-            </Text>
-            <Avatar
-              fallback="T"
-              radius="full"
-              size="5"
-              src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-            />
-          </Flex>
-        </Link>
-      ) : (
-        <LinkComponent
-          className="createStyle createHoverAnime"
-          href={getAuthorizePageUrl()}
-        >
-          <Flex align="center" gap="4">
-            <Text size="5" weight="bold">
-              ログイン
-            </Text>
-            <Avatar
-              fallback="T"
-              radius="full"
-              size="5"
-              src="https://api.iconify.design/ion:person-sharp.svg?color=%23ffffff"
-            />
-          </Flex>
-        </LinkComponent>
-      )}
+      </EsaAchievementsStyle>
+
+      <RightContents>
+        <LinkContents to="/ranking">
+          <Text as="div" size="5" weight="bold">
+            ranking
+          </Text>
+        </LinkContents>
+
+        <LinkContents to="/members">
+          <Text size="5" weight="bold">
+            members
+          </Text>
+        </LinkContents>
+
+        <LinkContents to="/achievements">
+          <Text size="5" weight="bold">
+            achievements
+          </Text>
+        </LinkContents>
+
+        {useStore($hasAuthenticated) ? (
+          <CreateStyle
+            className="createStyle createHoverAnime headerDefaultStyle"
+            to="/unlocked"
+          >
+            <Flex align="center" gap="4">
+              <Text size="5" weight="bold">
+                実績管理
+              </Text>
+              <Avatar
+                fallback="T"
+                radius="full"
+                size="5"
+                src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+              />
+            </Flex>
+          </CreateStyle>
+        ) : (
+          <LinkComponent
+            className="createStyle createHoverAnime"
+            href={getAuthorizePageUrl()}
+          >
+            <Flex align="center" gap="4">
+              <Text size="5" weight="bold">
+                ログイン
+              </Text>
+              <Avatar
+                fallback="T"
+                radius="full"
+                size="5"
+                src="https://api.iconify.design/ion:person-sharp.svg?color=%23ffffff"
+              />
+            </Flex>
+          </LinkComponent>
+        )}
+      </RightContents>
     </HeaderStyle>
   );
   // https://api.iconify.design/ion:add.svg
