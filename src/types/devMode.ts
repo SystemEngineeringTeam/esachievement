2a3685f46ce1316842fb15ca86bc6a086d2196d1
nasubi916 <r916nis@gmail.com>
2024-08-12 15:52:01 +0900

    もとに戻す

--- src/types/devMode.ts
+++
@@ -1,8 +0,0 @@
-import { type Member } from "./member";
-import { type UnlockedAchievement } from "./post-data/unlocked-achievements";
-
-export type DevMode = {
-  isDevMode: string;
-  members: Member[];
-  unlockedAchievements: UnlockedAchievement[];
-};
