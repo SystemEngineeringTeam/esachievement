14dbea28ee8641dc7538e013bc85d33e8e80c27d
nasubi916 <r916nis@gmail.com>
2024-08-14 04:57:26 +0900

    [object,Object]の不具合を修正

--- src/pages/auth/callback/index.tsx
+++ src/pages/auth/callback/index.tsx
@@ -5,10 +5,10 @@ import styled from "styled-components";
 import useSWR from "swr";
 import { match } from "ts-pattern";
 import { Center } from "@/components/Center";
-import { useAchievements } from "@/hooks/db/achievements";
-import { useUnlockedAchievements } from "@/hooks/db/unlocked-achievements";
+// import { useAchievements } from "@/hooks/db/achievements";
+// import { useUnlockedAchievements } from "@/hooks/db/unlocked-achievements";
 import { useMember } from "@/hooks/member";
-import { useTeam } from "@/hooks/teams";
+// import { useTeam } from "@/hooks/teams";
 import { S } from "@/lib/consts";
 import { requestAccessTokenData } from "@/lib/services/esa";
 import { $accessTokenData } from "@/lib/stores/auth";
@@ -60,8 +60,8 @@ export default function Page(): ReactElement {
   const navigate = useNavigate();
 
   async function fetchTokenAndTeams(): Promise<AccessTokenData> {
-    const { init: initAchievements } = useAchievements(useTeam);
-    const { init: initUnlockedAchievements } = useUnlockedAchievements(useTeam);
+    // const { init: initAchievements } = useAchievements(useTeam);
+    // const { init: initUnlockedAchievements } = useUnlockedAchievements(useTeam);
     if (accessTokenData != null) {
       // eslint-disable-next-line no-console
       console.warn("Access token has already been set");
@@ -75,8 +75,8 @@ export default function Page(): ReactElement {
     const tokenData = await requestAccessTokenData(code);
     $accessTokenData.set(tokenData);
 
-    await initAchievements();
-    await initUnlockedAchievements();
+    // await initAchievements();
+    // await initUnlockedAchievements();
 
     return tokenData;
   }
