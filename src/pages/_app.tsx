be92baeb6a6efb617b9930d860e6cb61c6b31c65
wappon28dev <wappon_28_dev@ruru.be>
2024-08-14 21:54:07 +0900

    💫 エレガントなエラー画面へ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,7 @@ export function Catch(): ReactElement {
   const error = useRouteError();
 
   const Error = styled(Center)`
+    position: relative;
     gap: 10px;
     color: #ff3e3e;
     background-color: #ffedee;
@@ -26,11 +27,20 @@ export function Catch(): ReactElement {
     }
   `;
 
+  const Image = styled.img`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    user-select: none;
+  `;
+
   return (
     <Error>
       <Icon height="3rem" icon="mdi:alert-circle" />
       <p>クライアントエラー発生！</p>
       <code>{String(error)}</code>
+      <Image src="https://i.gyazo.com/ebdf27f6d7df60165b7711e5e44d4388.webp" />
     </Error>
   );
 }
