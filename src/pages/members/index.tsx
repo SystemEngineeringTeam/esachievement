f6b529bdf0dacb349f140f8b39f09beead1c2a2c
nasubi916 <r916nis@gmail.com>
2024-09-02 22:45:25 +0900

    fix: fetcherの関数を追加

--- src/pages/members/index.tsx
+++ src/pages/members/index.tsx
@@ -9,7 +9,7 @@ import { useUnlockedAchievements } from "@/hooks/db/unlocked-achievements";
 import { useTeam } from "@/hooks/teams";
 import { S } from "@/lib/consts";
 import {
-  fetchMembersAndUnlockedAchievementsAndAchievements,
+  fetchMembersAndUnlockedAchievements,
   getUnlockedAchievementsFromMember,
 } from "@/lib/utils/fetchers";
 import { handleSWRError } from "@/lib/utils/swr";
@@ -26,7 +26,7 @@ export default function Page(): ReactElement {
   const swrMembersAndUnlockedAchievements = useSWRImmutable(
     "membersAndUnlockedAchievements",
     async () =>
-      await fetchMembersAndUnlockedAchievementsAndAchievements(
+      await fetchMembersAndUnlockedAchievements(
         fetchMembers,
         fetchUnlockedAchievements,
       ),
