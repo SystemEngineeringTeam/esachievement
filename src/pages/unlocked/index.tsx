f56ef71600c476bf7907198b2190237753f5e361
wappon28dev <wappon_28_dev@ruru.be>
2024-09-02 14:48:10 +0900

    ✨ UnlockableCard でのインタラクションを API コール

--- src/pages/unlocked/index.tsx
+++ src/pages/unlocked/index.tsx
@@ -1,14 +1,20 @@
 import { Box } from "@radix-ui/themes";
-import { type ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 import styled from "styled-components";
 import useSWRImmutable from "swr/immutable";
 import { match } from "ts-pattern";
 import { ErrorScreen } from "@/components/ErrorScreen";
 import { UnlockableCard } from "@/components/achievements/UnlockableCard";
 import { useAchievements } from "@/hooks/db/achievements";
+import { useUnlockedAchievements } from "@/hooks/db/unlocked-achievements";
+import { useMember } from "@/hooks/member";
 import { useTeam } from "@/hooks/teams";
 import { S } from "@/lib/consts";
+import { fetchAchievementsWithUnlocked } from "@/lib/utils/fetchers";
 import { handleSWRError } from "@/lib/utils/swr";
+import { type CurrentMember } from "@/types/member";
+import { type Achievement } from "@/types/post-data/achievements";
+import { type UnlockedAchievement } from "@/types/post-data/unlocked-achievements";
 
 const BoxStyle = styled(Box)`
   margin: 0 auto;
@@ -17,19 +23,96 @@ const BoxStyle = styled(Box)`
 `;
 
 export default function Page(): ReactElement {
-  const { fetch } = useAchievements(useTeam);
-  const swrFetchAchievements = useSWRImmutable("fetchAchievements", fetch);
+  const { fetchCurrentMember } = useMember();
+  const { fetch: fetchAchievements } = useAchievements(useTeam);
+  const { fetch: fetchUnlockedAchievements, update } =
+    useUnlockedAchievements(useTeam);
+  const swrAchievementsWithUnlocked = useSWRImmutable(
+    "achievementsWithUnlocked",
+    async () => {
+      const achievementsKit = await fetchAchievementsWithUnlocked(
+        fetchAchievements,
+        fetchUnlockedAchievements,
+      );
+      const currentMember = await fetchCurrentMember();
 
-  return match(swrFetchAchievements)
+      return {
+        ...achievementsKit,
+        currentMember,
+      };
+    },
+  );
+
+  const [isUILocked, setIsUILocked] = useState(false);
+
+  const handleUnlockToggle = async (
+    targetAchievementId: Achievement["id"],
+    unlockedAchievements: UnlockedAchievement[],
+    currentMember: CurrentMember,
+    isUnlocked: boolean,
+    mutate: () => Promise<void>,
+  ): Promise<void> => {
+    setIsUILocked(true);
+
+    try {
+      await update(
+        isUnlocked
+          ? [
+              ...unlockedAchievements,
+              {
+                achievementID: targetAchievementId,
+                createdAt: new Date(),
+                memberEmail: currentMember.email,
+              },
+            ]
+          : unlockedAchievements.filter(
+              ({ achievementID: id }) => id !== targetAchievementId,
+            ),
+      );
+      await mutate();
+    } finally {
+      setIsUILocked(false);
+    }
+  };
+
+  return match(swrAchievementsWithUnlocked)
     .with(S.Loading, () => <p>Loading...</p>)
-    .with(S.Success, ({ data }) => (
-      <BoxStyle width="70%">
-        <Box mt="20vh" />
-        {data?.map((achievement) => (
-          <UnlockableCard key={achievement.id} achievement={achievement} />
-        ))}
-      </BoxStyle>
-    ))
+    .with(
+      S.Success,
+      ({
+        data: { achievements, unlockedAchievements, currentMember },
+        mutate,
+      }) => (
+        <BoxStyle width="70%">
+          <Box mt="20vh" />
+          {achievements.map((achievement) => {
+            const isUnlocked = unlockedAchievements.some(
+              (u) => u.achievementID === achievement.id,
+            );
+
+            return (
+              <UnlockableCard
+                key={achievement.id}
+                achievement={achievement}
+                isDisabled={isUILocked}
+                isUnlocked={isUnlocked}
+                setIsUnlocked={(u) => {
+                  void handleUnlockToggle(
+                    achievement.id,
+                    unlockedAchievements,
+                    currentMember,
+                    u,
+                    async () => {
+                      await mutate();
+                    },
+                  );
+                }}
+              />
+            );
+          })}
+        </BoxStyle>
+      ),
+    )
     .otherwise(({ data, error }) => (
       <ErrorScreen error={handleSWRError(data, error)} />
     ));
--- src/types/member.ts
+++ src/types/member.ts
@@ -1,6 +1,11 @@
 import { type ArrayElem } from "./utils";
+import { type useMember } from "@/hooks/member";
 import { type useTeam } from "@/hooks/teams";
 
 export type Member = ArrayElem<
   Awaited<ReturnType<ReturnType<typeof useTeam>["fetchMembers"]>>
+>;
+
+export type CurrentMember = Awaited<
+  ReturnType<ReturnType<typeof useMember>["fetchCurrentMember"]>
 >;
