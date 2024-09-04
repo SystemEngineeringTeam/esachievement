5356d62b9a6eae7eef84e34a15b9fa46797aaa98
nasubi916 <r916nis@gmail.com>
2024-09-05 00:14:17 +0900

    style当てを忘れていた

--- src/components/team/Selector.tsx
+++ src/components/team/Selector.tsx
@@ -34,6 +34,65 @@ const DivStyled = styled.div`
   margin-top: 20px;
 `;
 
+
+const ButtonStyled = styled(Button)`
+  font-weight: 600;
+  font-family: sans-serif;
+  font-size: 1rem;
+
+  background-color: #e7e7e7;
+  color: #00cdc2;
+  border: 1px solid #00cdc2;
+
+  width: fit-content;
+  height: fit-content;
+
+  padding: 1.2vh 1.3vw 1.2vh 1.8vw;
+  margin-top: 4vh;
+  margin-left: 0.3vw;
+
+  border-radius: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  z-index: 1;
+
+  box-shadow:
+    6px 6px 16px #b5bec9,
+    -6px -6px 16px #ffffff;
+
+  transform-origin: 50% 50%;
+  transition: 300ms;
+
+  ::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 120%;
+    background-color: #00cdc2;
+
+    top: 0;
+    left: 0;
+    z-index: -1;
+    transform-origin: 100% 50%;
+    transform: scaleX(0%);
+    transition: transform 300ms;
+  }
+
+  &:hover {
+    box-shadow: none;
+    transform: scale(1.06);
+  }
+
+  &:hover ::after {
+    transform-origin: 0% 50%;
+    transform: scaleX(100%);
+    transform: none;
+  }
+`;
+
 export function TeamSelector(): ReactElement {
   const navigate = useNavigate();
   const { fetchJoinedTeams, markTeamNameAsSelected } = useMember();
@@ -60,14 +119,14 @@ export function TeamSelector(): ReactElement {
         </FlexStyled>
         <DivStyled>
           {teamName !== "" && (
-            <Button
+            <ButtonStyled
               onClick={() => {
                 navigate("/ranking");
               }}
               size="4"
             >
               {teamName}に参加する
-            </Button>
+            </ButtonStyled>
           )}
         </DivStyled>
       </DivCenter>
