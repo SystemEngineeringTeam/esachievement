1806f260b000c79ea34bd902c98a2764e43e271b
nasubi916 <r916nis@gmail.com>
2024-09-07 01:33:15 +0900

    info完成

--- src/pages/achievements/[id]/index.tsx
+++ src/pages/achievements/[id]/index.tsx
@@ -1,47 +1,59 @@
-import { Flex } from "@radix-ui/themes";
 import { type ReactElement } from "react";
+import { useParams } from "react-router-dom";
+import useSWRImmutable from "swr/immutable";
+import { match } from "ts-pattern";
+import { ErrorScreen } from "@/components/ErrorScreen";
 import { Info } from "@/components/achievements/Info";
+import { useAchievements } from "@/hooks/db/achievements";
+import { useUnlockedAchievements } from "@/hooks/db/unlocked-achievements";
+import { useTeam } from "@/hooks/teams";
+import { S } from "@/lib/consts";
+import { fetchMembersAndUnlockedAchievementsAndAchievements } from "@/lib/utils/fetchers";
+import { handleSWRError } from "@/lib/utils/swr";
 import { RecentUnlockedCard } from "@/components/achievements/RecentUnlockedCard";
 
-// export default function Page(): ReactElement {
-//   const { id } = useParams("/achievements/:id");
-//   const { fetch } = useAchievements(useTeam);
-//   const swrAchievement = useSWRImmutable("achievements", fetch);
-
-//   return match(swrAchievement)
-//     .with(S.Loading, () => <p>Loading...</p>)
-//     .with(S.Success, ({ data }) => (
-//       <div>
-//         {data.map((d) => {
-//           if (d.id === Number(id)) {
-//             return (
-//               <div key={d.id}>
-//                 <h1>name: {d.name}</h1>
-//                 <p>id: {d.id}</p>
-//                 <p>description: {d.description}</p>
-//                 <p>icon: {d.icon}</p>
-//                 <p>createdAt: {String(d.createdAt)}</p>
-//                 <p>updatedAt: {String(d.updatedAt)}</p>
-//                 <p>tags: {d.tags.join(", ")}</p>
-//               </div>
-//             );
-//           }
-//           return null;
-//         })}
-//       </div>
-//     ))
-//     .otherwise(({ data, error }) => (
-//       <ErrorScreen error={handleSWRError(data, error)} />
-//     ));
-// }
-
 export default function Page(): ReactElement {
-  return (
-    <Flex>
-      <Info />
-      <Flex overflow="scroll" width="100%">
-        <RecentUnlockedCard />
-      </Flex>
-    </Flex>
+  const { id } = useParams();
+  const { fetchMembers } = useTeam();
+  const { fetch: fetchAchievements } = useAchievements(useTeam);
+  const { fetch: fetchUnlockedAchievements } = useUnlockedAchievements(useTeam);
+  const swrMembersAndUnlockedAchievementsAndAchievements = useSWRImmutable(
+    "membersAndUnlockedAchievementsAndAchievements",
+    async () =>
+      await fetchMembersAndUnlockedAchievementsAndAchievements(
+        fetchMembers,
+        fetchAchievements,
+        fetchUnlockedAchievements,
+      ),
   );
+
+  return match(swrMembersAndUnlockedAchievementsAndAchievements)
+    .with(S.Loading, () => <p>Loading...</p>)
+    .with(
+      S.Success,
+      ({ data: { members, achievements, unlockedAchievements } }) => (
+        <>
+          {achievements.map((a) => {
+            if (a.id === Number(id)) {
+              const rateOfUnlocked = unlockedAchievements.filter(
+                (ua) => ua.achievementID === a.id,
+              ).length;
+              return (
+                <Info
+                  key={a.id}
+                  icon={a.icon}
+                  name={a.name}
+                  rateOfUnlocked={rateOfUnlocked}
+                  tags={a.tags}
+                />
+              );
+            }
+            return null;
+          })}
+        </>
+      ),
+    )
+    .otherwise(({ data, error }) => (
+      <ErrorScreen error={handleSWRError(data, error)} />
+    ));
 }
