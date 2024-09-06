7af0e830f07c81f17b06a79a26578d966a6142d1
kinako1415 <Owata09@outlook.jp>
2024-09-06 21:07:17 +0900

    色な作業

--- src/components/achievements/Info.tsx
+++ src/components/achievements/Info.tsx
@@ -1,35 +1,78 @@
 import { Flex, Text, Avatar, Box } from "@radix-ui/themes";
 import { type ReactElement } from "react";
+import styled from "styled-components";
+
+const InfoBox = styled(Flex)`
+  color: #242d3c;
+  position: relative;
+  left: 0;
+  background-color: #e7e7e7;
+  border-radius: 0 30px 30px 0;
+  height: 100vh;
+  width: fit-content;
+  padding: 9rem 4rem 6rem 4rem;
+  box-shadow:
+    6px 6px 16px #b5bec9,
+    -6px -6px 16px #ffffff;
+`;
+
+const PercentageUnlocked = styled(Box)`
+  color: #00cdc2;
+  font-weight: bold;
+  font-size: 5rem;
+  box-shadow:
+    6px 6px 16px #b5bec9,
+    -6px -6px 16px #ffffff;
+  padding: 0.8rem 2.4rem;
+  border-radius: 20px;
+`;
+
+const TagStyle = styled(Box)`
+  color: #242d3c;
+  font-weight: bold;
+  font-size: 1.2rem;
+  width: fit-content;
+  box-shadow:
+    6px 6px 16px #b5bec9,
+    -6px -6px 16px #ffffff;
+  padding: 0.2rem 1rem;
+  border-radius: 100rem;
+`;
+
+const TagGroup = styled(Flex)`
+  width: 14rem;
+  flex-wrap: wrap;
+`;
 
 export function Info(): ReactElement {
-  const achievementTest = {
-    id: 1,
-    name: "first blood",
-    description: "Get the first kill in a match.",
-    icon: "https://placehold.jp/150x150.png",
-    tag: ["kill"],
-  };
-
   return (
-    <Box maxWidth="8rem">
+    <InfoBox direction="column">
       <Flex align="center" direction="column">
         <Avatar
           fallback="T"
-          size="7"
+          size="9"
           src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
         />
 
-        <Text size="6" weight="bold">
+        <Text mt="1rem" size="8" weight="bold">
           恋愛失敗
         </Text>
       </Flex>
 
-      {achievementTest.tag.map((tag, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <Text key={index} color="gray">
-          タグ {tag}
-        </Text>
-      ))}
-    </Box>
+      <Flex direction="column" mt="2rem">
+        <Text weight="bold">タグ</Text>
+        <TagGroup>
+          <TagStyle mr="0.6rem" mt="1rem">
+            #Love
+          </TagStyle>
+          <TagStyle mt="1rem">#Love</TagStyle>
+        </TagGroup>
+      </Flex>
+
+      <Flex direction="column" mt="2rem">
+        <Text weight="bold">全体の実績解除率</Text>
+        <PercentageUnlocked mt="1rem">50%</PercentageUnlocked>
+      </Flex>
+    </InfoBox>
   );
 }
--- src/components/achievements/RecentUnlockedCard.tsx
+++ src/components/achievements/RecentUnlockedCard.tsx
@@ -1,77 +1,156 @@
-import { Avatar, Flex, Table, Text } from "@radix-ui/themes";
-import { useEffect, useState, type ReactElement } from "react";
-// import { useNavigate } from "react-router-dom";
+import { Avatar, Box, Flex, Text } from "@radix-ui/themes";
+import { type ReactElement } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-import Achievements from "@/assets/achievements.json";
-import { type UnlockedAchievement } from "@/types/post-data/unlocked-achievements";
+// import { useNavigate } from "react-router-dom";
 
-const TableRow = styled(Table.Row)`
-  transition: background-color 100ms;
-  cursor: pointer;
+// const TableRow = styled(Table.Row)`
+//   transition: background-color 100ms;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: #e2e8f0;
+//   }
+// `;
+
+// export function RecentUnlockedCard({
+//   unlockedAchievement,
+// }: {
+//   unlockedAchievement: UnlockedAchievement;
+// }): ReactElement {
+//   // const navigate = useNavigate();
+
+//   const [dayDiff, setDayDiff] = useState(0);
+//   const rtf1 = new Intl.RelativeTimeFormat("jp-lg", { style: "short" });
+
+//   const matchedAchievement = Achievements.achievements.find(
+//     (a) => a.id === unlockedAchievement.achievementID,
+//   );
+//   if (matchedAchievement == null) {
+//     throw new Error("matchedAchievement not found!");
+//   }
+
+//   useEffect(() => {
+//     const now = new Date();
+//     const formatedCreatedAt = new Date(matchedAchievement.createdAt);
+//     setDayDiff(
+//       Math.round(
+//         (formatedCreatedAt.getTime() - now.getTime()) / (24 * 60 * 60 * 1000),
+//       ),
+//     );
+//   });
+
+//   return (
+//     <TableRow
+//     // onClick={() => {
+//     //   navigate("/achievements/:id", {
+//     //     params: {
+//     //       id: matchedAchievement.id.toString(),
+//     //     },
+//     //   });
+//     // }}
+//     >
+//       <Table.RowHeaderCell>
+//         <Text color="gray" size="5">
+//           {rtf1.format(dayDiff, "day")}
+//         </Text>
+//         {/* <Text>{passedDate}</Text> */}
+//       </Table.RowHeaderCell>
+//       <Table.Cell>
+//         <Flex gap="2">
+//           <Avatar fallback="A" size="6" src={matchedAchievement?.icon} />
+//         </Flex>
+//       </Table.Cell>
+//       <Table.Cell>
+//         <Text as="div" size="8" weight="bold">
+//           {matchedAchievement?.name}
+//         </Text>
+//       </Table.Cell>
+//       <Table.Cell>{matchedAchievement?.description}</Table.Cell>
+//       <Table.Cell>
+//         <Text as="div" size="6">
+//           #{matchedAchievement?.tags[0].name}
+//         </Text>
+//       </Table.Cell>
+//     </TableRow>
+//   );
+// }
+
+const a = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 },
+];
+
+const UnlockedBox = styled(Flex)`
+  width: 100%;
+  height: 100%;
+  margin: 7rem 4rem 0rem 4rem;
+`;
+
+const UnlockedCardStyle = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  color: #242d3c;
+  width: 100%;
+  height: fit-content;
+  border-radius: 20px;
+  padding: 0rem 0.8rem;
+  box-shadow:
+    6px 6px 16px #b5bec9,
+    -6px -6px 16px #ffffff;
+`;
+
+const UnlockedCardList = styled(Flex)`
+  padding: 1rem 2rem;
+  border-radius: 10px;
+  transition: 100ms;
+  margin: 0 1rem;
   &:hover {
-    background-color: #e2e8f0;
+    box-shadow:
+      inset 6px 6px 32px #b5bec9,
+      inset -10px -10px 32px #ffffff;
   }
 `;
 
-export function RecentUnlockedCard({
-  unlockedAchievement,
-}: {
-  unlockedAchievement: UnlockedAchievement;
-}): ReactElement {
-  // const navigate = useNavigate();
+const UnlockedTitle = styled(Box)`
+  width: 100%;
+  font-weight: bold;
+  font-size: 1.2rem;
+`;
 
-  const [dayDiff, setDayDiff] = useState(0);
-  const rtf1 = new Intl.RelativeTimeFormat("jp-lg", { style: "short" });
-
-  const matchedAchievement = Achievements.achievements.find(
-    (a) => a.id === unlockedAchievement.achievementID,
-  );
-  if (matchedAchievement == null) {
-    throw new Error("matchedAchievement not found!");
-  }
-
-  useEffect(() => {
-    const now = new Date();
-    const formatedCreatedAt = new Date(matchedAchievement.createdAt);
-    setDayDiff(
-      Math.round(
-        (formatedCreatedAt.getTime() - now.getTime()) / (24 * 60 * 60 * 1000),
-      ),
-    );
-  });
-
+export function RecentUnlockedCard(): ReactElement {
   return (
-    <TableRow
-    // onClick={() => {
-    //   navigate("/achievements/:id", {
-    //     params: {
-    //       id: matchedAchievement.id.toString(),
-    //     },
-    //   });
-    // }}
-    >
-      <Table.RowHeaderCell>
-        <Text color="gray" size="5">
-          {rtf1.format(dayDiff, "day")}
-        </Text>
-        {/* <Text>{passedDate}</Text> */}
-      </Table.RowHeaderCell>
-      <Table.Cell>
-        <Flex gap="2">
-          <Avatar fallback="A" size="6" src={matchedAchievement?.icon} />
-        </Flex>
-      </Table.Cell>
-      <Table.Cell>
-        <Text as="div" size="8" weight="bold">
-          {matchedAchievement?.name}
-        </Text>
-      </Table.Cell>
-      <Table.Cell>{matchedAchievement?.description}</Table.Cell>
-      <Table.Cell>
-        <Text as="div" size="6">
-          #{matchedAchievement?.tags[0].name}
-        </Text>
-      </Table.Cell>
-    </TableRow>
+    <UnlockedBox direction="column">
+      <UnlockedTitle mb="2rem" ml="20px">
+        最近解除した人
+      </UnlockedTitle>
+      <UnlockedCardStyle to="/member">
+        <Box height="1rem" width="1rem" />
+        {a.map((b) => (
+          <UnlockedCardList key={b.id} align="center">
+            <Avatar
+              fallback="T"
+              mr="2rem"
+              size="5"
+              src="https://images.unsplash.com/photo-1607346256330-dee7af15f7c5?&w=64&h=64&dpr=2&q=70&crop=focalpoint&fp-x=0.67&fp-y=0.5&fp-z=1.4&fit=crop"
+            />
+            <Text mr="3rem" size="7" weight="bold">
+              恋愛失敗
+            </Text>
+            <Box mr="3rem" width="20rem">
+              dafjasdklflkasdjflkjasdflkjlaskjflaskdj
+            </Box>
+            <Text>#love</Text>
+          </UnlockedCardList>
+        ))}
+        <Box height="1rem" width="1rem" />
+      </UnlockedCardStyle>
+    </UnlockedBox>
   );
 }
