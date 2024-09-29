789ada9216c161fde41617674dd7fc1f4bebde9f
nasubi916 <r916nis@gmail.com>
2024-09-30 01:00:30 +0900

    createDummyStyle

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import { Flex } from "@radix-ui/themes";
 import { type ReactElement } from "react";
 import styled from "styled-components";
-import { Link } from "@/router.ts";
+// import { Link } from "@/router.ts";
 
 const FooterStyle = styled(Flex)`
   position: fixed;
@@ -15,8 +15,30 @@ const FooterStyle = styled(Flex)`
   z-index: 10;
 `;
 
-const CreateStyle = styled(Link)`
-  background-color: #00cdc2;
+// const CreateStyle = styled(Link)`
+//   background-color: #00cdc2;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: fit-content;
+//   height: fit-content;
+//   padding: 1rem;
+//   margin-left: auto;
+//   border-radius: 80px;
+//   box-shadow:
+//     12px 12px 16px #b5bec9,
+//     -12px -12px 16px #ffffff;
+//   overflow: hidden;
+//   transition: 160ms;
+
+//   &:hover {
+//     box-shadow: none;
+//     width: fit-content;
+//   }
+// `;
+
+const CreateDummyStyle = styled.div`
+  background-color: #5e5e5e;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -30,11 +52,6 @@ const CreateStyle = styled(Link)`
     -12px -12px 16px #ffffff;
   overflow: hidden;
   transition: 160ms;
-
-  &:hover {
-    box-shadow: none;
-    width: fit-content;
-  }
 `;
 
 const PrusStyle = styled.img`
@@ -45,12 +62,12 @@ const PrusStyle = styled.img`
 export function Footer(): ReactElement {
   return (
     <FooterStyle>
-      <CreateStyle to="/create">
+      <CreateDummyStyle>
         <PrusStyle
           alt="prus"
           src="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 512 512'%3E%3Cpath fill='none' stroke='%23f7f7f7' stroke-linecap='round' stroke-linejoin='round' stroke-width='32' d='M256 112v288m144-144H112'/%3E%3C/svg%3E"
         />
-      </CreateStyle>
+      </CreateDummyStyle>
     </FooterStyle>
   );
 }
